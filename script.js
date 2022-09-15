155d96f9548806e65f0a71c9baf3a2f05b3c2555
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-09-15 12:05:21 +0600

    Error count inside the red box fixed

--- script.js
+++ script.js
@@ -45,6 +45,8 @@ const typeController = (e) => {
   if (newLetterCorrect) {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
+      // Error fixed -  Error count is increased for showing inside the red area
+    errorCount++;
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
   }
 
